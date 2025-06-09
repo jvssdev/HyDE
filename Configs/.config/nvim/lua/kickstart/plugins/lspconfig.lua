ef8a41f537f72a25f393f55df36dbe57738754c8
jvssdev <jvssdev@hotmail.com>
2025-06-09 15:14:29 -0300

    language servers

--- Configs/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ Configs/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -211,18 +211,20 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
+        clangd = {},
+        gopls = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
+        zls = {},
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+        ts_ls = {},
+        astro = {
+          cmd = { "bunx", "astro-ls", "--stdio" },
+        },
 
         lua_ls = {
           -- cmd = { ... },
